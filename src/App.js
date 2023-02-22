3ea2b431f18d3d5e5ae527775435170c74c12bce
Rowell Jay Rodriguez <rodriguez.rowelljay@gmail.com>
2023-02-22 14:28:15 +0800

    Travel Journal using props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./components/Card"
+import Nav from "./components/Nav"
+import Data from "./Data"
 
 function App() {
+  const getData = Data.map((e)=> {
+    return <Card 
+            key={e.id}
+            item={e}
+          />
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <section className="card-rows">
+        {getData}
+      </section>
     </div>
   );
 }
